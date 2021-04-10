3824c77b502486e0fa4777d9c5ca764c6262dce5
JoaoGaBRiel-X <joaogabriel.mc@hotmail.com>
2021-04-10 15:42:52 -0300

    Fim capitulo 15

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,7 +5,10 @@ from django.http import HttpRequest
 from unittest import skip
 
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+	DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+	ExistingListItemForm, ItemForm,
+)
 
 class HomePageTest(TestCase):
 
@@ -31,6 +34,12 @@ class ListViewTest(TestCase):
 		response = self.client.get(f'/lists/{correct_list.id}/')
 
 		self.assertEqual(response.context['list'], correct_list)
+
+	def test_displays_item_form(self):
+		list_ = List.objects.create()
+		response = self.client.get(f'/lists/{list_.id}/')
+		self.assertIsInstance(response.context['form'], ExistingListItemForm)
+		self.assertContains(response, 'name="text"')
 
 	def test_displays_only_items_for_that_list(self):
 		correct_list = List.objects.create()
@@ -87,19 +96,12 @@ class ListViewTest(TestCase):
 
 	def test_for_invalid_input_passes_form_to_template(self):
 		response = self.post_invalid_input()
-		self.assertIsInstance(response.context['form'], ItemForm)
+		self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
 	def test_for_invalid_input_shows_error_on_page(self):
 		response = self.post_invalid_input()
 		self.assertContains(response, EMPTY_ITEM_ERROR)
 
-	def test_displays_item_form(self):
-		list_ = List.objects.create()
-		response = self.client.get(f'/lists/{list_.id}/')
-		self.assertIsInstance(response.context['form'], ItemForm)
-		self.assertContains(response, 'name="text"')
-
-	@skip
 	def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
 		list1 = List.objects.create()
 		item1 = Item.objects.create(list=list1, text='textey')
@@ -108,7 +110,7 @@ class ListViewTest(TestCase):
 			data={'text': 'textey'}
 		)
 
-		expected_error = 'Você já incluiu este item em sua lista'
+		expected_error = DUPLICATE_ITEM_ERROR
 		self.assertContains(response, expected_error)
 		self.assertTemplateUsed(response, 'list.html')
 		self.assertEqual(Item.objects.all().count(), 1)
