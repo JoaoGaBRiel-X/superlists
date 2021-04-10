54f534ad15c9ea42f008cd98ed18ece795b8b34f
JoaoGaBRiel-X <joaogabriel.mc@hotmail.com>
2021-04-10 00:32:38 -0300

    rename all item input ids and names. still broken

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -18,7 +18,7 @@ class NewVisitorTest(FunctionalTest):
 		self.assertIn('To-Do', header_text)
 
 		# Ela é convidada a inserir um item de tarefa imediatamente
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
@@ -36,7 +36,7 @@ class NewVisitorTest(FunctionalTest):
 		# Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
 		# item. Ela insere "Use peacock feathers to make fly" (Usar penas de pavão
 		# para fazer um flu - Edith é bem metódica)
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		inputbox.send_keys('Use peacock feathers to make fly')
 		inputbox.send_keys(Keys.ENTER)
 
@@ -50,7 +50,7 @@ class NewVisitorTest(FunctionalTest):
 
 		# Edith inicia uma nova lista de tarefas
 		self.browser.get(self.live_server_url)
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
@@ -74,7 +74,7 @@ class NewVisitorTest(FunctionalTest):
 
 		# Francis inicia uma nova lista inserindo um item novo. Ele é menos
 		# interessante que Edith...
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		inputbox.send_keys('Buy milk')
 		inputbox.send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy milk')
